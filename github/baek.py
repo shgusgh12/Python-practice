afdbe28c0dec4a16afb12191ef53a039aa4df755
shgusgh12 <shgusgh12@naver.com>
2022-04-27 00:49:51 +0900

    11050 백준

--- github/baek.py
+++ github/baek.py
@@ -1,8 +1,14 @@
 
-a = [1,1,1,2,2,3,3,3,4,4,5]
-aSet = set(a)
-b = list(aSet)
 
+n , k = map(int,input().split())
+def fact(a):
+    if a==0:
+        return 1
+    if a == 1 :
+        return 1
+    else :
+        return a * fact(a-1)
 
-print(b)
-print(a)
+sum = fact(n)//(fact(n-k)*fact(k))  
+print(sum)
+
