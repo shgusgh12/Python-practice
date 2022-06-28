bc3ecbaf5d1ff62a05947eab06ba06485c5151c5
shgusgh12 <shgusgh12@naver.com>
2022-06-28 20:07:52 +0900

    0628

--- github/baekjoon/2920.py
+++ github/baekjoon/2920.py
@@ -1,17 +1,25 @@
-a = list(map(int,input().split() ))
-x =0
-j = 8
-k = 0
-for i in range(1,9):
-    if a[i-1] ==i:
+a = list(map(int, input().split()))
+for i in range(1,8):
+    if a[i-1] == i:
         x = 1
-for i in range(1,9):
-    for k in range(8,0,-1):
-        if a[i-1] == k:
+    else:
+        x = 0
+        break
+i = 0
+if x != 1: 
+    for j in range(8,1,-1):
+        if a[i] == j:
+            i = i+1
             x = 2
+        else:
+            x = 0
+            break
+        
+
 if x == 1:
     print('ascending')
 elif x==2:
-    print('decending')
-else :
-    print('mixed')
+    print('decending') 
+else:
+    print('mixed')    
+    
