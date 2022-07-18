b964616cdd30c7186a08e434de89fecee6dcf092
shgusgh12 <shgusgh12@naver.com>
2022-07-18 22:40:19 +0900

    0718

--- github/baekjoon/10816.py
+++ github/baekjoon/10816.py
@@ -4,4 +4,22 @@ M = int(input())
 t = list(map(int, input().split(' ')))
 cards.sort
 
-#def binsearch():
+def binsearch(target):
+    low = 0
+    high = N-1
+    mid = (low + high)//2
+    while low <= high:
+        if cards[mid] == target:
+            return mid
+        elif cards[mid] < target:
+            high = mid - 1
+        else:
+            low = mid + 1
+arr = [] 
+cnt = 0
+for i in range(M):
+    if binsearch(t[i]) > 0:
+        cnt += 1
+        arr[i] = cnt
+    
+
