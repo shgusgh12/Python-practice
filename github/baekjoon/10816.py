407b8b9c3defe9782d60899e79bf3ff729e37abe
shgusgh12 <shgusgh12@naver.com>
2022-07-19 14:40:49 +0900

    0719

--- github/baekjoon/10816.py
+++ github/baekjoon/10816.py
@@ -1,25 +1,31 @@
+
+
+def binsearch(l, target, start, end):
+    if start > end:
+        return 0
+    mid = (start + end) //2
+    if l[mid] == target:
+        return l[start:end+1].count(target)
+    elif l[mid] > target:
+        binsearch(l, target, start, mid-1)
+    else:
+        binsearch(l, target, mid+1, end)
 N = int(input())
 cards = list(map(int, input().split(' ')))
 M = int(input())
 t = list(map(int, input().split(' ')))
-cards.sort
 
-def binsearch(target):
-    low = 0
-    high = N-1
-    mid = (low + high)//2
-    while low <= high:
-        if cards[mid] == target:
-            return mid
-        elif cards[mid] < target:
-            high = mid - 1
-        else:
-            low = mid + 1
-arr = [] 
-cnt = 0
-for i in range(M):
-    if binsearch(t[i]) > 0:
-        cnt += 1
-        arr[i] = cnt
-    
 
+cnt = {} #hashing을 딕셔너리로 함
+
+for i in cards:   #for문에서 i 값은 리스트의 첫번째 값을 의미한다 0이아님
+    if i in cnt:
+        cnt[i] += 1 #키값은 그대로 들어가고 value값증가한다 ex) 10:1 이런식
+    else:
+        cnt[i] = 1 
+
+for i in t:
+    if i in cnt:
+        print(cnt[i], end=' ')
+    else:
+        print(0, end=' ')
