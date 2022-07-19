10565fc290d7eea7604e7d29e89f59090bb52944
shgusgh12 <shgusgh12@naver.com>
2022-07-19 18:21:27 +0900

    #

---
+++ github/ds/sum.py
@@ -0,0 +1,11 @@
+def change(lst, idx, val):
+    lst[idx] = val
+
+x = [11, 22, 33, 44, 55]
+print('x =', x)
+
+index = int(input())
+value = int(input())
+
+change(x, index, value)
+print(f'x = {x}')
