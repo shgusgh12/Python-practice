b844eaf05a69e0c0dc0a3a3b33b4de2bdb5d7736
shgusgh12 <shgusgh12@naver.com>
2022-07-20 17:52:29 +0900

    0720

--- github/baekjoon/10989.py
+++ github/baekjoon/10989.py
@@ -1,11 +1,13 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 arr = [0] * 10000
 
 for i in range(N):
     a= int(input())
-    arr[a-1] += 1    #index에 해당하는 숫자를 증가시킴
+    arr[a] += 1    #index에 해당하는 숫자를 증가시킴
 
 for i in range(10000):
     if arr[i] != 0:
         for j in range(arr[i]):
-            print(i+1)
+            print(i)
