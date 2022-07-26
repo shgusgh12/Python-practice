008cec2b7ee071d095d9dcc9cf476f34b3245e77
shgusgh12 <shgusgh12@naver.com>
2022-07-26 16:00:35 +0900

    0726

---
+++ github/baekjoon/1654.py
@@ -0,0 +1,25 @@
+k , N = map(int, input().split(' '))
+# x 값을 정해놓고 점점 늘려가면서 abcd 값을 더해 N에 가까워지면 출력
+arr = []
+for i in range(k):
+    arr.append(int(input()))
+start = 0
+x = 0
+min = 0
+for i in range(k):
+    if arr[i] > min:
+        min = arr[i]
+end = min
+sum = 0
+while start +1 < end:
+    mid = (start + end) // 2
+    
+    for i in range(k):
+        if arr[i] >= mid:
+            sum += arr[i] // mid 
+    if sum >= N:
+        start = mid
+    else:
+        end = mid
+    sum = 0
+print (start)
