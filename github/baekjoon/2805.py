15fb09cf56cf36046b561d5fa3a9fa9d9d6abcb2
shgusgh12 <shgusgh12@naver.com>
2022-07-26 15:32:13 +0900

    0726

--- github/baekjoon/2805.py
+++ github/baekjoon/2805.py
@@ -1,18 +1,34 @@
+#매개변수 탐색
+
 N , M = map(int, input().split(' '))
 
 arr = map(int, input().split(' '))
 arr = list(arr)
-arr2 = sorted(arr)
+max = 0
+for i in range(N):
+    if arr[i] > max :
+        max =arr[i]
+start = 0  #나무를 얻을 수 있는 경우
+end = max   #나무를 얻을 수 없는 경우
 sum = 0
-h = 1
 
-while True:
+def check(height, arr, sum, M):
+    
     for i in range(N):
-        sum += arr2[i] - h
-    if sum == M :
-        break
+        if arr[i] >= height:
+            sum += arr[i] - height
+    
+    if sum >= M:
+        return True
     else:
-        h+=1
-        sum=0 
+        return False
 
-print(h)
+while start + 1< end:
+    mid = (start + end)//2 
+    
+    if check(mid, arr, sum, M):
+        start = mid   #mid를 체크했는데 sum이 더 큰 경우  
+    else:
+        end = mid   #sum 보다 작은경우
+    
+print(start)
