3a306884607f3d69d158366b89333416cfac8844
shgusgh12 <shgusgh12@naver.com>
2022-07-27 14:53:51 +0900

    0727

--- github/baekjoon/1436.py
+++ github/baekjoon/1436.py
@@ -1,1 +1,11 @@
-N = int(input())
+n = int(input())
+num = 666
+cnt = 0
+
+while True:
+    if '666' in str(num):   
+        cnt+=1              
+    if cnt == n:           
+        print(num)          
+        break               
+    num+=1           
