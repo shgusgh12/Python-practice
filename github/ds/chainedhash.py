c45647a1ac4a492b2ffbd3feb8041e8bcb2c2361
shgusgh12 <shgusgh12@naver.com>
2022-08-04 20:04:06 +0900

    #

--- github/ds/chainedhash.py
+++ github/ds/chainedhash.py
@@ -17,5 +17,27 @@ class ChainedHash:
         if isinstance(key, int):
             return key % self.capacity
         return (int(hashlib.sha256(str(key).encode()).hexdigest(), 16)% self.capacity)
+   
+    def search(self, key: Any) -> Any:
+        hash = self.hash_value(key)
+        p = self.table[hash]
 
+        while p is not None:
+            if p.key == key:
+                return p.value
+            p = p.next
+        
+        return None
+    def add(self, key:Any, value: Any)-> bool:
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return False
+            p = p.next
+        
+        temp = Node(key, value, self.table[hash])
+        self.table[hash] = temp
+        return True
     
