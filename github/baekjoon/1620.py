e6161ab5e34cae9fa31e455cf40adae280dfd6fb
shgusgh12 <shgusgh12@naver.com>
2022-08-11 00:09:56 +0900

    #

--- github/baekjoon/1620.py
+++ github/baekjoon/1620.py
@@ -3,12 +3,12 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 arr = {}
 for i in range(N):
-    key = input().strip
+    key = input().rstrip()
     arr[key] = i
     arr[i] = key
 for j in range(M):
-    ans = input().strip()
-    if ans.isdigit():      #숫자 구분해주는 함수
+    ans = input().rstrip()
+    if ans.isdigit():      #숫자 구분해주는 함수 input으로 받아도 숫자인지 아닌지 구분해줌
         print(arr[int(ans)-1])
     else:
         print(arr[ans]+1)
