26c34a4d61e5d64ed89428b5ee04db41b7b720b5
shgusgh12 <84013707+shgusgh12@users.noreply.github.com>
2023-01-05 00:21:47 +0900

    에라토스테네스의 체

--- github/programmers/소수찾기.py
+++ github/programmers/소수찾기.py
@@ -1,17 +1,22 @@
 import math
-#모든 True 는 소수
-#에라토스테네스의 체 활용
-def solution(n):
+def solution(nums):
+    ans = []
     answer = 0
-    arr = [True for i in range(n+1)]
-    m = int(math.sqrt(n)) + 1
-    
-    for i in range(2, m):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            for k in range(j+1 , len(nums)):
+                ans.append(nums[i] + nums[j] + nums[k])
+    maxid = max(ans)
+    arr = [True] * (maxid + 1)
+    m = int(math.sqrt(maxid)) + 1
+
+    for i in range(2,m):
         if arr[i] == True:
-            for j in range (2*i, n+1, i):
+            for j in range(2*i, maxid+1, i):
                 arr[j] = False
-    for i in range(n+1):
-        if arr[i] == True:
+    for j in range(len(ans)):
+        if arr[ans[j]] == True:
             answer += 1
-    answer -= 2
+
+
     return answer
