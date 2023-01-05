e293aa5a7694240fd8cba74b4bee7d40677e633b
shgusgh12 <84013707+shgusgh12@users.noreply.github.com>
2023-01-05 14:58:48 +0900

    Update 실패율.py

--- github/programmers/실패율.py
+++ github/programmers/실패율.py
@@ -2,20 +2,25 @@ def solution(N, stages):
     user = len(stages)
     answer = []
     ans = []
-    sta = [0 for _ in range(N+2)] 
-    cnt1 = 0
-    
-    for j in range(user):
-        for k in range(1,N+2):
-            if stages[j] == k:
-                sta[k] += 1
     old = 0
     for i in range(1,N+1):
-        old = sta[i]
-        percent = sta[i] / user
-        answer.append(percent)
-        user -= old
-    #인덱스로 바꾸는 법
-        
+        cnt = 0
+        for j in range(len(stages)):
+            if stages[j] == i:
+                 cnt+=1
+        if cnt == 0:
+            answer.append(0)
+        else:
+            percent = cnt / user
+            answer.append(percent)
+        user -= cnt
+    ans = sorted(answer, reverse=True)
+    answer1 = [] 
+    for i in range(len(ans)):
+        #index 함수 => 그 자리에 맞는 index 변환해줌
+        answer1.append(answer.index(ans[i])+1)
+        answer[answer.index(ans[i])] =2   #실패율을 2로 변경해서 중복으로 걸리지 않게 하기
     
-    return answer
+            
+        
+    return answer1
