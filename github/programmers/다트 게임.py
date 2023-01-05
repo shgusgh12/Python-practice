1af47bec059656cfdb366424e4f6ee6860f0f77a
shgusgh12 <84013707+shgusgh12@users.noreply.github.com>
2023-01-05 23:56:49 +0900

    Update 다트 게임.py

--- github/programmers/다트 게임.py
+++ github/programmers/다트 게임.py
@@ -1,7 +1,6 @@
-#isnumeric 함수 숫자인지 판단해주는 함수
-
 def solution(dartResult):
     n =''
+    answer = 0
     score = []
     for i in dartResult:
         if i.isnumeric():
@@ -10,8 +9,25 @@ def solution(dartResult):
             n = int(n) **1
             score.append(n)
             n = ''
+        elif i == 'D':
+            n = int(n)**2
+            score.append(n)
+            n = ''
+        elif i == 'T':
+            n = int(n) **3
+            score.append(n)
+            n = ''
+        elif i == '*': 
+            #-1 인덱스 활용하여 뒤에서부터 생각
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
+            else:
+                score[-1] = score[-1] * 2
+        elif i == '#':
+            score[-1] = score[-1] * -1
+                
+    answer += score[0] + score[1] + score[2]
     
     
-    
-    answer = 0
     return answer
