6688034ce29ce536530ed9c3d91edf8cb29eec69
shgusgh12 <shgusgh12@naver.com>
2023-03-24 02:14:50 +0900

    #

--- github/baekjoon/1463.py
+++ github/baekjoon/1463.py
@@ -1,25 +1,17 @@
-import sys
+n = int(input())
 
-n = int(sys.stdin.readline())
-answer = 0
-while True:
+arr = [0] * (n+1)
+#arr에는 index가 1이 되기 위한 횟수의 최솟값이 들어간다
 
-    if n % 2 ==0: # 짝수 
-        if n % 3 == 1:
-            n = n -1 
-            answer += 1
-        else:
-            n = n //2
-            answer += 1
-    else: #홀수 
-        if n % 3 == 0:
-            n = n//3
-            answer+=1
-        else:
-            n = n - 1
-            answer += 1
+for i in range(2, n+1):
+    arr[i] = arr[i-1] + 1
+    # arr[i-1]에서 하나만 더 연산하면 arr[i] 가 될수 있기 때문에 1을 더해준다
+    if i % 2 == 0:
+        arr[i] = min(arr[i], arr[i//2]+1)
+
+    if i % 3 == 0:
+        arr[i] = min(arr[i], arr[i//3] + 1)
+
     
-    if n == 1:
-        break
-print(answer)
-    
+print(arr[n])
+ 
--- github/practice.py
+++ github/practice.py
@@ -1,180 +1,68 @@
-from email.quoprimime import body_check
-from operator import concat
-from winreg import HKEY_CURRENT_CONFIG
 
 
-a = 100, 5 , 'k'
-print('"더이상 카레먹기 싫어요"')
-#is 연산자 
-#== 연산자는 값을 비교하는 연산자
-#값을 비교하는 것이 아니라 변수가 가리키고 있는 곳 비교
-a = [1,2,3,4,5]
-b = a
-c = [1,2,3,4,5]
+arr = [-2, -1, 1, -3, 3, 5]
+n = 6
 
-print('a is b = ', a is b)
-print('a is c= ', a is c)
-print('b is c = ',b is c)
-a = "korea"
-b += "!"
-c = b[:-1]
-#슬라이싱 하는 순간 새롭게 할당된다.
-print ('a is c =', a is c)
+# O(n^3) 0번째 index 부터 값을 하나씩 늘려가면서 배열의 합을 구한다
+# MaxSum에 최대값을 저장 
+def MaxSubarray1(arr, n): 
+    MaxSum = 0
+    for i in range(n):
+        for j in range(i,n):
+            ThisSum = 0
+            for k in range(i,j+1):
+                ThisSum = ThisSum + arr[k]
+            MaxSum = max(MaxSum, ThisSum)
+    return MaxSum
 
-#elif else 조건문 
-age = 20
+def MaxSubarray2(arr, n):
+    MaxSum = 0
+    for i in range(n):
+        ThisSum = 0
+        for j in range(i,n):
+            ThisSum = ThisSum + arr[j]
+            MaxSum = max(MaxSum, ThisSum)
+    return MaxSum
 
-if age < 30:
-    print("청년이에요")
-elif age < 60:
-    print("중년")
-else:
-    print("노년")
+def MaxSubarray3(arr, left, right):
+    
+    if left == right:
+        if arr[left] > 0:
+            return arr[left]
+        else:
+            return 0
 
-#for 반복문
-for asdf in range(10):
-    print(asdf)
+    center = (left + right) // 2
+    MaxLeft = MaxSubarray3(arr, left, center)
+    MaxRight = MaxSubarray3(arr, center+1, right)
+    MaxCenterL = 0
+    ThisSum = 0
+    for i in range(center, left-1, -1):
+        ThisSum = ThisSum + arr[i]
+        MaxCenterL = max(MaxCenterL, ThisSum)
 
-#이중 for문 별찍기
+    MaxCenterR = 0
+    ThisSum = 0
 
-for i in range(10,0,-1):
-    for j in range(i):
-        print("*", end='')
-    print("")
+    for i in range(center+1, right+1):
+        ThisSum = ThisSum + arr[i]
+        MaxCenterR = max(MaxCenterR, ThisSum)
+    
+    MaxCenter = MaxCenterL + MaxCenterR
+    MaxSum = max(max(MaxLeft, MaxRight), MaxCenter)
+    return MaxSum
 
-
-#리스트 end 옵션에 \t 추가하면 tab 된 상태로 출력
-lst = ['A','B','C','D','E']
-for i in range(len(lst)):
-    print(lst[i], end='\t')
-print("")
-
-# #반대로 출력시 lst[처음인덱스:어디까지출력할건지:출력방향및간격 :]
-# for i in lst[0:3:1]:
-#     print(i, end=' ')
-
-#리스트 안의 리스트 값 출력
-a = [100, 3.14, 'korea', [100, 200,300], (100, 200 ,300)]
-print(a[4][2])
-
-for i in range(len(a)):
-    print(a[i], end=' ')
-print()
+print(MaxSubarray3(arr,0,n-1))
 
 
-#정렬 sort() 함수 사용
-b = [4,2,5,2,4]
-# b.sort(reverse=True) #내림차순
-# for i in range(len(b)):
-#     print(b[i])
 
-for i in range(-4, -1):
-    print(b[i])
+def move(n, start=1, end=3, via=2):
+    if n == 1:
+        print(start, '에서', end, '으로 1번 원반 이동')
+    else:
+        move(n-1, start, via, end)
+        print(start, '에서', end, '으로', n, '번 원반 이동')
+        move(n-1, via, end, start)
 
-#리스트에 추가 삭제
-b.append(1)
-for i in range(len(b)):
-    print(b[i], end=' ')
-print()
-del b[-1]
-for i in range(len(b)):
-    print(b[i], end=' ')
-print()
-
-# #리스트 병합  더하기 연산자 튜플도 가능
-# a = [0,1,2,3,4]
-# b = [5,6,7,8,9] 
-# print(a*3)
-
-# animals = ['elephant', 'hippo','lion','tiger','alligator']
-# name = input('동물의 이름을 입력하시오 ')
-# index = animals.index(name)
-# print(f'{name} 동물의 케이지는 {index}번 위치에 있습니다')
-
-#중복 제거
-lst = [1,2,3,4,6,7,7,7]
-lst2 = set(lst)
-lst3 = list(lst2)
-print(lst3, type(lst3))
-
-#list comprehension
-b = []
-for i in range(1,11):
-    b.append(i)
-
-c = [i*i for i in range(1,11)]
-print(c)
-c = [i*i for i in range(1,11)if i!= 5]
-print(c)
-c = [i for i in range(1,51)if i%2 != 0]
-print(c)
-
-def func(i):
-    i = i*2
-    return i
-c = [func(i) for i in [1,2,3,4,5]]
-print(c)
-
-#중첩 반복문 튜플로 묶어라 하나의 값은 안묶어도 된다
-a = [(i, j) for i in range(1,3) for j in range(1,10)]
-print(a)
-
-#딕셔너리 인덱스 접근 불가능
-b = {}
-b['id'] ='hong'
-b['id'] = 'kim' #나중에 들어온 key값으로 대체됨
-b['name']= '홍길동'
-b['age'] = 20
-b['address'] = (1,4,2,4) #리스트도 가능
-print(b)
-#key값만 출력
-for i in b.keys(  ):
-    print(i, end=' ')
-print()
-#value만 출력
-for i in b.values():
-    print(i, end=' ')
-print()
-#key, value 모두 출력 ====> tuple로 묶인다
-for i in b.items():
-    print(i , type(i))
-#key가 인덱스의 역할을 한다
-print(b['id'])
-#중첩 dictionary
-b2 = {
-    'name' : 'jake',
-    'age' : 30,
-    'pastime' : {
-        'reading': 30,
-        'walking': 60
-    }
-}
-print(b2['pastime']['walking'])
-#삽입 
-b2['age'] = 30
-print(b2['age'])
-#삭제 
-del b2['age']
-print(b2)
-
-# print('이름을 입력하세요: ', end='')
-# name = input()
-# print(f'안녕하세요? {name}님')
-
-# n = int(input())
-# i = 1
-# sum = 0
-# while i <= n:
-#     sum += i
-#     i = i+1
-
-#튜플 언패킹
-fruits = ('apple','banana','grapes')
-(mine, *yours)= fruits
-print(yours) #yours는 list 형태로 저장된다
-#중복허용 안하면서 순서가 중요한경우 set사용 못할때
-mylist = ['a','b','c','c','c']
-dic = dict.fromkeys(mylist) #value 값들은 전부 none으로 표시됨
-print(dic)
-mylist = list(dic)
-print (mylist)
+n = int(input('원반의 개수: '))
+move(n)
