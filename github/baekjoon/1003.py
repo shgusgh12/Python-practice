272edd2535ed7f7933f2ddfd52ed53d838ce0e27
shgusgh12 <shgusgh12@naver.com>
2023-03-26 16:35:22 +0900

    dp

--- github/baekjoon/1003.py
+++ github/baekjoon/1003.py
@@ -1,20 +1,23 @@
 t = int(input())
 
-def fibo(k):
-    if k == 0:
-        return 0
-    elif k == 1:
-        return 1
-    else:
-        return fibo(k-1) + fibo(k-2)
+
 
 for i in range(t):
-    n = int(input())
-    a = 0
-    b=0
-    if fibo(n) == 0:
-        a +=1
-    else:
-        b+=1
-    print(a , b)
+    index0 = [0] * 41
+    index1 = [0] * 41
+    index0[0] = 1
+    index0[1] = 0
+    index1[0] = 0
+    index1[1] = 1
 
+    fibo = int(input())
+    for j in range(2, fibo+1):
+        if index0[j] == 0 and index1[j]==0:
+            index0[j] = index0[j-2] + index0[j-1]
+            index1[j] = index1[j-2] + index1[j-1]
+    print(index0[fibo], index1[fibo])
+    
+
+
+        
+    
