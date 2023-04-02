d79c51fa54beaa29ba91bb209f4495e09fd56cc0
shgusgh12 <shgusgh12@naver.com>
2023-04-02 16:25:23 +0900

    #

--- github/baekjoon/9095.py
+++ github/baekjoon/9095.py
@@ -1,2 +1,26 @@
-T = int(input())
-arr= [int(input()) for i in range(T)]
+t = int(input())
+
+
+
+
+
+for _ in range(t):
+    num = int(input())
+    
+    
+    dp = [0] * (11)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    
+    for i in range(4,num+1):
+        answer = 0
+        for j in range(1,4):
+            answer += dp[i-j]
+        dp[i] = answer
+
+    print(dp[num])  
+
+
+
